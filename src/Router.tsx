47ec470c37164efba59d5a70d6802cfbed11d545
Jane <duddms72003@naver.com>
2024-02-27 13:14:20 +0900

    add BrowserRouter basename

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,7 +6,7 @@ interface IRouterProps {}
 
 function Router({}: IRouterProps) {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Characters />} />
         <Route path="/:characterId" element={<Character />} />
